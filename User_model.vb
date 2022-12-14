d09bfaf7dbe5057cea7353644f4fe5f7676fa96a
dev4ult <nibrasbiasaaja@gmail.com>
2022-12-14 21:59:27 +0700

    tambah user model untuk ddl table user

---
+++ User_model.vb
@@ -0,0 +1,16 @@
+﻿Public Class User_model
+    Private db As Database
+
+    Public Sub New()
+        db = New Database()
+    End Sub
+
+    Public Function GetAllUserAsDataTable() As DataTable
+        db.Query("SELECT * FROM users")
+
+        Dim list_user As New DataTable
+        list_user.Load(db.Fetch())
+
+        Return list_user
+    End Function
+End Class
