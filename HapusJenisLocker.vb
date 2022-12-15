7e7d6664d559fb1af881bd43371e71d633e9c66c
GerrysatriaH <gerrysatria10@gmail.com>
2022-12-15 15:18:11 +0700

    Menambahkan Class HapusJenisLocker

---
+++ HapusJenisLocker.vb
@@ -0,0 +1,6 @@
+﻿Public Class HapusJenisLocker
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        FormJenisLocker.Show()
+        Me.Close()
+    End Sub
+End Class
