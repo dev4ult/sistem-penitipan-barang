528d251974f99f0344519649899374d791f09d96
GerrysatriaH <gerrysatria10@gmail.com>
2022-12-15 18:11:59 +0700

    Menambahkan Class Jenis_Locker.vb

---
+++ Jenis_Locker.vb
@@ -0,0 +1,24 @@
+﻿Public Class Jenis_Locker
+
+    Private ukuran As String
+    Private biaya As Integer
+
+    'Getter & Setter 
+    Public Property UkuranJenisLockerProperty() As String
+        Get
+            Return ukuran
+        End Get
+        Set(ByVal value As String)
+            ukuran = value
+        End Set
+    End Property
+
+    Public Property BiayaJenisLockerProperty() As Integer
+        Get
+            Return biaya
+        End Get
+        Set(ByVal value As Integer)
+            biaya = value
+        End Set
+    End Property
+End Class
