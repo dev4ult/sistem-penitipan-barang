7154fe95938acbfb599b59979d085481b3d1ada5
GerrysatriaH <gerrysatria10@gmail.com>
2022-12-15 15:09:40 +0700

    Menghapus File Jenis Locker.vb

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Sistem_Penitipan_Barang.JenisLocker
+            Me.MainForm = Global.Sistem_Penitipan_Barang.FormJenisLocker
         End Sub
     End Class
 End Namespace
