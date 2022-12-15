a423c0d4aff44a449203b17cb489603d610a4a1a
GerrysatriaH <gerrysatria10@gmail.com>
2022-12-15 15:11:40 +0700

    Menambahkan Class FormJenisLocker

---
+++ FormJenisLocker.vb
@@ -0,0 +1,25 @@
+﻿Public Class FormJenisLocker
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub BtnCreate_Click(sender As Object, e As EventArgs) Handles BtnCreate.Click
+        TambahJenisLocker.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles BtnUpdate.Click
+        UpdateJenisLocker.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        HapusJenisLocker.Show()
+        Me.Hide()
+    End Sub
+End Class
