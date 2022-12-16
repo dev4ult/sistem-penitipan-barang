2c52108c53d66f356db8fbb0edc87adc0298b477
FaresAriandha <AkunKoding2009@gmail.com>
2022-12-16 22:13:52 +0700

    Tambah elemen NumericUpDown

--- FormSewa.Designer.vb
+++ FormSewa.Designer.vb
@@ -29,7 +29,6 @@ Partial Class FormSewa
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.TxtBoxWaktuSewa = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
@@ -48,6 +47,9 @@ Partial Class FormSewa
         Me.RTBKetUser = New System.Windows.Forms.RichTextBox()
         Me.Label19 = New System.Windows.Forms.Label()
         Me.LblJumlahKomentar = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.NUDLamaSewa = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NUDLamaSewa, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -65,7 +67,7 @@ Partial Class FormSewa
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label3.Location = New System.Drawing.Point(123, 257)
+        Me.Label3.Location = New System.Drawing.Point(338, 198)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(114, 25)
         Me.Label3.TabIndex = 4
@@ -94,7 +96,7 @@ Partial Class FormSewa
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label2.Location = New System.Drawing.Point(358, 98)
+        Me.Label2.Location = New System.Drawing.Point(123, 198)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(128, 25)
         Me.Label2.TabIndex = 19
@@ -104,7 +106,7 @@ Partial Class FormSewa
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Me.Label4.Location = New System.Drawing.Point(460, 293)
+        Me.Label4.Location = New System.Drawing.Point(436, 138)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(41, 20)
         Me.Label4.TabIndex = 22
@@ -114,24 +116,18 @@ Partial Class FormSewa
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label6.Location = New System.Drawing.Point(358, 257)
+        Me.Label6.Location = New System.Drawing.Point(338, 98)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(116, 25)
         Me.Label6.TabIndex = 21
         Me.Label6.Text = "Lama Sewa"
         '
-        'TxtBoxWaktuSewa
-        '
-        Me.TxtBoxWaktuSewa.Location = New System.Drawing.Point(363, 291)
-        Me.TxtBoxWaktuSewa.Name = "TxtBoxWaktuSewa"
-        Me.TxtBoxWaktuSewa.Size = New System.Drawing.Size(91, 22)
-        Me.TxtBoxWaktuSewa.TabIndex = 20
-        '
         'Label7
         '
         Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Bisque
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label7.Location = New System.Drawing.Point(339, 419)
+        Me.Label7.Location = New System.Drawing.Point(361, 379)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(116, 25)
         Me.Label7.TabIndex = 24
@@ -141,7 +137,7 @@ Partial Class FormSewa
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Me.Label8.Location = New System.Drawing.Point(339, 444)
+        Me.Label8.Location = New System.Drawing.Point(361, 404)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(84, 20)
         Me.Label8.TabIndex = 25
@@ -151,7 +147,7 @@ Partial Class FormSewa
         '
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label9.Location = New System.Drawing.Point(459, 432)
+        Me.Label9.Location = New System.Drawing.Point(481, 392)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(41, 25)
         Me.Label9.TabIndex = 26
@@ -161,17 +157,18 @@ Partial Class FormSewa
         '
         Me.LblBiayaPerJam.AutoSize = True
         Me.LblBiayaPerJam.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.LblBiayaPerJam.Location = New System.Drawing.Point(494, 432)
+        Me.LblBiayaPerJam.Location = New System.Drawing.Point(516, 392)
         Me.LblBiayaPerJam.Name = "LblBiayaPerJam"
-        Me.LblBiayaPerJam.Size = New System.Drawing.Size(45, 25)
+        Me.LblBiayaPerJam.Size = New System.Drawing.Size(23, 25)
         Me.LblBiayaPerJam.TabIndex = 27
-        Me.LblBiayaPerJam.Text = "100"
+        Me.LblBiayaPerJam.Text = "0"
         '
         'Label10
         '
         Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.Color.Bisque
         Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label10.Location = New System.Drawing.Point(339, 486)
+        Me.Label10.Location = New System.Drawing.Point(361, 446)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(110, 25)
         Me.Label10.TabIndex = 28
@@ -181,17 +178,17 @@ Partial Class FormSewa
         '
         Me.LblTotalBiaya.AutoSize = True
         Me.LblTotalBiaya.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.LblTotalBiaya.Location = New System.Drawing.Point(494, 486)
+        Me.LblTotalBiaya.Location = New System.Drawing.Point(516, 446)
         Me.LblTotalBiaya.Name = "LblTotalBiaya"
-        Me.LblTotalBiaya.Size = New System.Drawing.Size(45, 25)
+        Me.LblTotalBiaya.Size = New System.Drawing.Size(23, 25)
         Me.LblTotalBiaya.TabIndex = 30
-        Me.LblTotalBiaya.Text = "100"
+        Me.LblTotalBiaya.Text = "0"
         '
         'Label13
         '
         Me.Label13.AutoSize = True
         Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label13.Location = New System.Drawing.Point(459, 486)
+        Me.Label13.Location = New System.Drawing.Point(481, 446)
         Me.Label13.Name = "Label13"
         Me.Label13.Size = New System.Drawing.Size(41, 25)
         Me.Label13.TabIndex = 29
@@ -201,7 +198,7 @@ Partial Class FormSewa
         '
         Me.BtnYesSewa.Cursor = System.Windows.Forms.Cursors.Arrow
         Me.BtnYesSewa.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.BtnYesSewa.Location = New System.Drawing.Point(348, 581)
+        Me.BtnYesSewa.Location = New System.Drawing.Point(350, 575)
         Me.BtnYesSewa.Name = "BtnYesSewa"
         Me.BtnYesSewa.Size = New System.Drawing.Size(191, 55)
         Me.BtnYesSewa.TabIndex = 31
@@ -212,7 +209,7 @@ Partial Class FormSewa
         '
         Me.BtnNoSewa.Cursor = System.Windows.Forms.Cursors.Arrow
         Me.BtnNoSewa.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.BtnNoSewa.Location = New System.Drawing.Point(128, 581)
+        Me.BtnNoSewa.Location = New System.Drawing.Point(130, 575)
         Me.BtnNoSewa.Name = "BtnNoSewa"
         Me.BtnNoSewa.Size = New System.Drawing.Size(191, 55)
         Me.BtnNoSewa.TabIndex = 32
@@ -223,7 +220,7 @@ Partial Class FormSewa
         '
         Me.LsbLockerTersedia.FormattingEnabled = True
         Me.LsbLockerTersedia.ItemHeight = 16
-        Me.LsbLockerTersedia.Location = New System.Drawing.Point(364, 131)
+        Me.LsbLockerTersedia.Location = New System.Drawing.Point(129, 231)
         Me.LsbLockerTersedia.Name = "LsbLockerTersedia"
         Me.LsbLockerTersedia.Size = New System.Drawing.Size(135, 100)
         Me.LsbLockerTersedia.TabIndex = 33
@@ -231,18 +228,20 @@ Partial Class FormSewa
         'LblNamaLocker
         '
         Me.LblNamaLocker.AutoSize = True
+        Me.LblNamaLocker.BackColor = System.Drawing.SystemColors.Control
         Me.LblNamaLocker.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.LblNamaLocker.Location = New System.Drawing.Point(259, 419)
+        Me.LblNamaLocker.Location = New System.Drawing.Point(281, 379)
         Me.LblNamaLocker.Name = "LblNamaLocker"
-        Me.LblNamaLocker.Size = New System.Drawing.Size(44, 25)
+        Me.LblNamaLocker.Size = New System.Drawing.Size(42, 25)
         Me.LblNamaLocker.TabIndex = 36
-        Me.LblNamaLocker.Text = "A-1"
+        Me.LblNamaLocker.Text = "null"
         '
         'Label16
         '
         Me.Label16.AutoSize = True
+        Me.Label16.BackColor = System.Drawing.Color.Bisque
         Me.Label16.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label16.Location = New System.Drawing.Point(123, 419)
+        Me.Label16.Location = New System.Drawing.Point(145, 379)
         Me.Label16.Name = "Label16"
         Me.Label16.Size = New System.Drawing.Size(118, 25)
         Me.Label16.TabIndex = 34
@@ -252,17 +251,18 @@ Partial Class FormSewa
         '
         Me.LblLamaSewa.AutoSize = True
         Me.LblLamaSewa.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.LblLamaSewa.Location = New System.Drawing.Point(259, 486)
+        Me.LblLamaSewa.Location = New System.Drawing.Point(281, 446)
         Me.LblLamaSewa.Name = "LblLamaSewa"
         Me.LblLamaSewa.Size = New System.Drawing.Size(23, 25)
         Me.LblLamaSewa.TabIndex = 38
-        Me.LblLamaSewa.Text = "4"
+        Me.LblLamaSewa.Text = "0"
         '
         'Label17
         '
         Me.Label17.AutoSize = True
+        Me.Label17.BackColor = System.Drawing.Color.Bisque
         Me.Label17.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Label17.Location = New System.Drawing.Point(123, 486)
+        Me.Label17.Location = New System.Drawing.Point(145, 446)
         Me.Label17.Name = "Label17"
         Me.Label17.Size = New System.Drawing.Size(116, 25)
         Me.Label17.TabIndex = 37
@@ -272,7 +272,7 @@ Partial Class FormSewa
         '
         Me.Label18.AutoSize = True
         Me.Label18.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Me.Label18.Location = New System.Drawing.Point(124, 511)
+        Me.Label18.Location = New System.Drawing.Point(146, 471)
         Me.Label18.Name = "Label18"
         Me.Label18.Size = New System.Drawing.Size(84, 20)
         Me.Label18.TabIndex = 39
@@ -280,7 +280,7 @@ Partial Class FormSewa
         '
         'RTBKetUser
         '
-        Me.RTBKetUser.Location = New System.Drawing.Point(131, 298)
+        Me.RTBKetUser.Location = New System.Drawing.Point(346, 239)
         Me.RTBKetUser.Name = "RTBKetUser"
         Me.RTBKetUser.Size = New System.Drawing.Size(171, 67)
         Me.RTBKetUser.TabIndex = 40
@@ -290,7 +290,7 @@ Partial Class FormSewa
         '
         Me.Label19.AutoSize = True
         Me.Label19.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
-        Me.Label19.Location = New System.Drawing.Point(132, 368)
+        Me.Label19.Location = New System.Drawing.Point(347, 309)
         Me.Label19.Name = "Label19"
         Me.Label19.Size = New System.Drawing.Size(156, 17)
         Me.Label19.TabIndex = 41
@@ -301,17 +301,36 @@ Partial Class FormSewa
         Me.LblJumlahKomentar.AutoSize = True
         Me.LblJumlahKomentar.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.LblJumlahKomentar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.LblJumlahKomentar.Location = New System.Drawing.Point(308, 337)
+        Me.LblJumlahKomentar.Location = New System.Drawing.Point(523, 278)
         Me.LblJumlahKomentar.Name = "LblJumlahKomentar"
         Me.LblJumlahKomentar.Size = New System.Drawing.Size(34, 25)
         Me.LblJumlahKomentar.TabIndex = 42
         Me.LblJumlahKomentar.Text = "50"
         '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label11.Location = New System.Drawing.Point(362, 471)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(119, 20)
+        Me.Label11.TabIndex = 43
+        Me.Label11.Text = "(Belum denda)"
+        '
+        'NUDLamaSewa
+        '
+        Me.NUDLamaSewa.Location = New System.Drawing.Point(341, 136)
+        Me.NUDLamaSewa.Name = "NUDLamaSewa"
+        Me.NUDLamaSewa.Size = New System.Drawing.Size(89, 22)
+        Me.NUDLamaSewa.TabIndex = 44
+        '
         'FormSewa
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(686, 706)
+        Me.ClientSize = New System.Drawing.Size(690, 706)
+        Me.Controls.Add(Me.NUDLamaSewa)
+        Me.Controls.Add(Me.Label11)
         Me.Controls.Add(Me.LblJumlahKomentar)
         Me.Controls.Add(Me.Label19)
         Me.Controls.Add(Me.RTBKetUser)
@@ -332,7 +351,6 @@ Partial Class FormSewa
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.TxtBoxWaktuSewa)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.CBUkuranLocker)
@@ -340,6 +358,7 @@ Partial Class FormSewa
         Me.Controls.Add(Me.Label1)
         Me.Name = "FormSewa"
         Me.Text = "FormSewa"
+        CType(Me.NUDLamaSewa, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -352,7 +371,6 @@ Partial Class FormSewa
     Friend WithEvents Label2 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents TxtBoxWaktuSewa As TextBox
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
     Friend WithEvents Label9 As Label
@@ -371,4 +389,6 @@ Partial Class FormSewa
     Friend WithEvents RTBKetUser As RichTextBox
     Friend WithEvents Label19 As Label
     Friend WithEvents LblJumlahKomentar As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents NUDLamaSewa As NumericUpDown
 End Class
