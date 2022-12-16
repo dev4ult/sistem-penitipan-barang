b86d6025d69dfc5028e88dd86195cb61683e1dcc
Kyasa422 <kyasa422@gmail.com>
2022-12-16 08:33:43 +0700

    commit test

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -37,7 +37,7 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(320, 204)
+        Me.TextBox1.Location = New System.Drawing.Point(316, 204)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 22)
         Me.TextBox1.TabIndex = 1
