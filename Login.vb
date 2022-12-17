98f15efee0f1be0a88c117422ecc8d75e180faa5
dev4ult <nibrasbiasaaja@gmail.com>
2022-12-17 13:19:53 +0700

    Menambahkan fungsi untuk set pesam error

--- Login.vb
+++ Login.vb
@@ -9,17 +9,26 @@
         ' Add any initialization after the InitializeComponent() call.
     End Sub
 
+    Public Sub SetFlashMessage(msg As String)
+        LBFMError.Text = msg
+        LBFMError.Visible = True
+    End Sub
+
 
     Private Sub BTNLogin_Click(sender As Object, e As EventArgs) Handles BTNLogin.Click
         Dim umail = TXTUmail.Text
         Dim password = TXTPassword.Text
 
         If umail = "" Or password = "" Then
-            MessageBox.Show("Input tidak bersifat kosong")
+            SetFlashMessage("Input tidak boleh bersifat kosong")
         Else
             If user_model.ValidateLogin(umail, password) Then
                 MessageBox.Show("Login berhasil")
             End If
         End If
+    End Sub
+
+    Private Sub LinkLBSignup_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLBSignup.LinkClicked
+        Signup.Show()
     End Sub
 End Class
--- Signup.vb
+++ Signup.vb
@@ -6,6 +6,7 @@ Public Class Signup
     Private username As String
     Private email As String
     Private password As String
+    Private passwordConfirmation As String
 
     Public Sub New()
         ' This call is required by the designer.
@@ -15,15 +16,25 @@ Public Class Signup
         user_model = New User_model
     End Sub
 
-    Private Sub BTNSignUp_Click(sender As Object, e As EventArgs) Handles BTNSignUp.Click
+    Public Sub SetFlashMessage(msg As String)
+        LBFMError.Text = msg
+        LBFMError.Visible = True
+    End Sub
+
+    Private Sub BTNSignUp_Click(sender As Object, e As EventArgs)
         username = TXTUsername.Text
         email = TXTEmail.Text
         password = TXTPassword.Text
+        passwordConfirmation = TXTPasswordConfirmation.Text
 
         If username = "" Or email = "" Or password = "" Then
-            MessageBox.Show("Input tidak bisa bersifat kosong")
-        ElseIf user_model.ValidateSignUp(username, email, password) Then
+            SetFlashMessage("Input tidak boleh bersifat kosong")
+        ElseIf user_model.ValidateSignUp(username, email, password, passwordConfirmation) Then
             MessageBox.Show("Berhasil menambahkan user baru")
         End If
+    End Sub
+
+    Private Sub LinkLBLogin_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLBLogin.LinkClicked
+        Login.Show()
     End Sub
 End Class
