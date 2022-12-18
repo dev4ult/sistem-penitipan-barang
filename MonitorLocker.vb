5a86ff759110a9fe9f979f2f9fca68a736a3ad63
kyasa422 <kyasa422@gmail.com>
2022-12-18 19:48:20 +0700

    perubahan kecil

--- MonitorLocker.vb
+++ MonitorLocker.vb
@@ -1,8 +1,7 @@
 ﻿Public Class MonitorLocker
 
     Private locker_model As Locker_model
-
-
+    Public selectedLocker As String
 
     Public Sub New()
 
@@ -13,25 +12,40 @@
 
 
         locker_model = New Locker_model
+
+        selectedLocker = ""
         ReloadDataGridLocker()
     End Sub
 
 
     Private Sub btntambahlocker_Click(sender As Object, e As EventArgs) Handles btntambahlocker.Click
-        Dim tambah = New TambahLocker
-        tambah.Show()
+        TambahLocker.Show()
     End Sub
 
     Private Sub btnhapuslocker_Click(sender As Object, e As EventArgs) Handles btnhapuslocker.Click
-        Dim hapus = New HapusLocker
-        hapus.Show()
-
+        If selectedLocker = "" Then
+            MessageBox.Show("Pilih locker terlebih dahulu")
+        Else
+            HapusLocker.Show()
+        End If
     End Sub
 
     Public Sub ReloadDataGridLocker()
         DataGridLocker.DataSource = locker_model.FetchAllLocker()
-
     End Sub
 
+    Private Sub DataGridLocker_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridLocker.CellClick
+        If Not e.RowIndex = -1 Then
+            Dim dataRows = DataGridLocker.Rows(e.RowIndex)
+            selectedLocker = dataRows.Cells(e.ColumnIndex).Value
+        End If
+    End Sub
 
+    Private Sub btnupdatestatus_Click(sender As Object, e As EventArgs) Handles btnupdatestatus.Click
+        If selectedLocker = "" Then
+            MessageBox.Show("Pilih locker terlebih dahulu")
+        Else
+            UpdateStatusLocker.Show()
+        End If
+    End Sub
 End Class
