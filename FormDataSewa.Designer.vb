dc4865ff26339bc7d987ac5e2f003ad3f3b1456e
FaresAriandha <AkunKoding2009@gmail.com>
2022-12-19 19:33:04 +0700

    Tambah Form Data Sewa

---
+++ FormDataSewa.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDataSewa
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DGV_DataSewa = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnTambahDataSewa = New System.Windows.Forms.Button()
+        Me.BtnUbahDataSewa = New System.Windows.Forms.Button()
+        Me.BtnHapusDataSewa = New System.Windows.Forms.Button()
+        CType(Me.DGV_DataSewa, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DGV_DataSewa
+        '
+        Me.DGV_DataSewa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV_DataSewa.Location = New System.Drawing.Point(12, 118)
+        Me.DGV_DataSewa.Name = "DGV_DataSewa"
+        Me.DGV_DataSewa.RowHeadersWidth = 51
+        Me.DGV_DataSewa.RowTemplate.Height = 24
+        Me.DGV_DataSewa.Size = New System.Drawing.Size(1101, 187)
+        Me.DGV_DataSewa.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(246, 39)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "List Data Sewa"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BtnTambahDataSewa
+        '
+        Me.BtnTambahDataSewa.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.BtnTambahDataSewa.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.BtnTambahDataSewa.Location = New System.Drawing.Point(12, 478)
+        Me.BtnTambahDataSewa.Name = "BtnTambahDataSewa"
+        Me.BtnTambahDataSewa.Size = New System.Drawing.Size(191, 55)
+        Me.BtnTambahDataSewa.TabIndex = 32
+        Me.BtnTambahDataSewa.Text = "Tambah Data"
+        Me.BtnTambahDataSewa.UseVisualStyleBackColor = True
+        '
+        'BtnUbahDataSewa
+        '
+        Me.BtnUbahDataSewa.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.BtnUbahDataSewa.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.BtnUbahDataSewa.Location = New System.Drawing.Point(209, 478)
+        Me.BtnUbahDataSewa.Name = "BtnUbahDataSewa"
+        Me.BtnUbahDataSewa.Size = New System.Drawing.Size(191, 55)
+        Me.BtnUbahDataSewa.TabIndex = 33
+        Me.BtnUbahDataSewa.Text = "Edit Data"
+        Me.BtnUbahDataSewa.UseVisualStyleBackColor = True
+        '
+        'BtnHapusDataSewa
+        '
+        Me.BtnHapusDataSewa.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.BtnHapusDataSewa.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.BtnHapusDataSewa.Location = New System.Drawing.Point(406, 478)
+        Me.BtnHapusDataSewa.Name = "BtnHapusDataSewa"
+        Me.BtnHapusDataSewa.Size = New System.Drawing.Size(191, 55)
+        Me.BtnHapusDataSewa.TabIndex = 34
+        Me.BtnHapusDataSewa.Text = "Hapus Data"
+        Me.BtnHapusDataSewa.UseVisualStyleBackColor = True
+        '
+        'FormDataSewa
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1139, 545)
+        Me.Controls.Add(Me.BtnHapusDataSewa)
+        Me.Controls.Add(Me.BtnUbahDataSewa)
+        Me.Controls.Add(Me.BtnTambahDataSewa)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DGV_DataSewa)
+        Me.Name = "FormDataSewa"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FormDataSewa"
+        CType(Me.DGV_DataSewa, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DGV_DataSewa As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnTambahDataSewa As Button
+    Friend WithEvents BtnUbahDataSewa As Button
+    Friend WithEvents BtnHapusDataSewa As Button
+End Class
---
+++ FormDataSewa.vb
@@ -0,0 +1,38 @@
+﻿Public Class FormDataSewa
+    Private sewa_model As Sewa_model
+    Private selectedLocker As String
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        sewa_model = New Sewa_model()
+        reloadRentData()
+    End Sub
+
+    Public Sub reloadRentData()
+        DGV_DataSewa.DataSource = sewa_model.getAllRentData()
+    End Sub
+
+    Private Sub FormDataSewa_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        reloadRentData()
+    End Sub
+
+    Private Sub DGV_DataSewa_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV_DataSewa.CellClick
+        Dim index As Integer = e.RowIndex
+        Dim selectedRow As DataGridViewRow
+        selectedRow = DGV_DataSewa.Rows(index)
+
+        selectedLocker = selectedRow.Cells(0).Value
+    End Sub
+
+    Private Sub BtnHapusDataSewa_Click(sender As Object, e As EventArgs) Handles BtnHapusDataSewa.Click
+        RemoveRentData.LblRemoveLockerName.Text = selectedLocker
+        RemoveRentData.Show()
+    End Sub
+
+    Private Sub BtnTambahDataSewa_Click(sender As Object, e As EventArgs) Handles BtnTambahDataSewa.Click
+        SewaLocker.Show()
+    End Sub
+End Class
