eac9feb2b78f1bfb367b36973dc30d2191d3284f
dev4ult <nibrasbiasaaja@gmail.com>
2022-12-22 00:24:12 +0700

    fungsionalitas untuk halaman list data penyewaan

---
+++ FormListDataPenyewaan.vb
@@ -0,0 +1,49 @@
+﻿Public Class FormListDataPenyewaan
+
+    Private selectedLocker As String
+    Private selectedTanggalSewa As Date
+    Private sewa_model As Sewa_model
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        sewa_model = New Sewa_model()
+
+        ReloadRentData()
+    End Sub
+
+    Public WriteOnly Property SetSelectedLocker()
+        Set(value)
+            selectedLocker = value
+        End Set
+    End Property
+
+    Public Sub ReloadRentData()
+        DGV_DataSewa.DataSource = sewa_model.FetchAllRentData()
+    End Sub
+
+    Private Sub BTNHapusSewa_Click(sender As Object, e As EventArgs) Handles BTNHapusSewa.Click
+        If Not selectedLocker = "" Then
+            RemoveRentData.LblRemoveLockerName.Text = selectedLocker
+            RemoveRentData.SetTanggalSewa = selectedTanggalSewa
+            RemoveRentData.Show()
+        End If
+    End Sub
+
+    Private Sub DGV_DataSewa_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV_DataSewa.CellClick
+        If Not e.RowIndex = -1 Then
+            Dim selectedRow = DGV_DataSewa.Rows(e.RowIndex)
+            selectedLocker = selectedRow.Cells(0).Value
+            selectedTanggalSewa = Date.Parse(selectedRow.Cells(2).Value)
+        End If
+    End Sub
+
+    Private Sub BTNKembali_Click(sender As Object, e As EventArgs) Handles BTNKembali.Click
+        AdminMenu.Show()
+        Me.Close()
+    End Sub
+
+End Class
